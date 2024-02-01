168517420bd9561dfc092e66374b8d0096a31967
Gabrielle <ms.grodarte@gmail.com>
2024-02-01 13:14:23 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.round(Math.random() * 10)
+
+const num3 = 25
+const num4 = 7
+const mod = num3 % num4
+
+let max = Math.max(12, 20, 18, 5, 19, 2)
